0a47b0db215cb33c576d7a593f12dbfcce7c8c2c
Elcan <fxraisen@gmail.com>
2025-11-04 09:20:15 +0300

    Create Lab5S2.java

---
+++ Lab5S2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Lab5S2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Bir sayı giriniz: ");
+        int n = input.nextInt();
+        
+        n = Math.abs(n);
+        
+        int result = sumDigits(n);
+        System.out.println("Basamakların toplamı: " + result);
+        
+        input.close();
+    }
+
+    public static int sumDigits(int n) {
+        if (n < 10) {
+            return n; 
+        } else {
+            return (n % 10) + sumDigits(n / 10); 
+        }
+    }
+}
