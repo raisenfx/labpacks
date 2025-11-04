45651fa54b30aeb791cc7571259c1ffa367b0065
Elcan <fxraisen@gmail.com>
2025-11-04 09:19:14 +0300

    Create Lab5S1.java

---
+++ Lab5S1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Lab5S1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int n = input.nextInt();
+
+        int result = factorial(n);
+        System.out.println(n + "! = " + result);
+
+        input.close();
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
