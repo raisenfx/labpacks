b52b091320abbb503d455ad5b1999c86af056712
Elcan <fxraisen@gmail.com>
2025-11-25 09:04:52 +0300

    Add files via upload

---
+++ MainS1.java
@@ -0,0 +1,14 @@
+public class MainS1 {
+    public static void main(String[] args) {
+
+
+        Student s1 = new Student();
+        s1.printStudentInfo();
+
+        System.out.println("------------");
+
+        Student s2 = new Student("Eljan", 18);
+        s2.printStudentInfo();
+    }
+}
+
---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student {
+
+    String name;
+    int age;
+
+    public Student() {
+        name = "Default Name";
+        age = 18;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void printStudentInfo() {
+        System.out.println("Name = " + name);
+        System.out.println("Age = " + age);
+    }
+
+}
